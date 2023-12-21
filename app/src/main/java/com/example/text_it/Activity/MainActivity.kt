c7d4fb53f9554b6e12232b3ef31635541e2895a3
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2023-12-21 15:32:12 +0530

    Revert "Issue:#92"

---
+++ app/src/main/java/com/example/text_it/Activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.text_it.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.text_it.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "meme"
+rootProject.name = "Text-it"
 include(":app")
  

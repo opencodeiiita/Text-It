de047db7fc3a0e2cb001b5542339454b5c296608
Rohit Pandey <120464365+Rohit-110@users.noreply.github.com>
2023-12-25 00:01:10 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,12 +27,17 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding=true
+        ;
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
         jvmTarget = "1.8"
+        ;
     }
 }
 

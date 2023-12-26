47d5d6dd8cf7cb585a3d74ed08ab96e586b0543d
Apoorv Mittal <apoorvmitthu@gmail.com>
2023-12-27 00:16:22 +0530

    Corrected Navbar

--- app/src/main/java/com/example/text_it/Activity/baseHomeActivity.kt
+++ app/src/main/java/com/example/text_it/Activity/baseHomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.text_it.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import com.example.text_it.Call
 import com.example.text_it.Contact
@@ -22,7 +23,7 @@ class baseHomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        bottomNavView = findViewById(R.id.bottomNavView)
+        bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavView)
 
         // Set the initial fragment
         supportFragmentManager.beginTransaction()
@@ -31,14 +32,30 @@ class baseHomeActivity : AppCompatActivity() {
 
         // Handle bottom navigation item clicks
         bottomNavView.setOnNavigationItemSelectedListener { menuItem ->
+//            if (menuItem.isChecked) {
+//                return@setOnNavigationItemSelectedListener false
+//            }
+
+//            for (i in 0 until bottomNavView.menu.size()) {
+//                val ithItem = bottomNavView.menu.getItem(i)
+//                ithItem.icon?.setTintList(getColorStateList(R.color.bottom_navbar_not_selected))
+//            }
+
             when (menuItem.itemId) {
                 R.id.message -> replaceFragment(MessageFragment())
                 R.id.call -> replaceFragment(Call())
                 R.id.contact -> replaceFragment(Contact())
                 R.id.setting -> replaceFragment(Setting())
             }
+
+//            menuItem.icon?.setTintList(getColorStateList(R.color.bottom_navbar_selected))
+
             true
         }
+
+
+
+
     }
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
